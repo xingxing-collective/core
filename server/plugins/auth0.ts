ba5abb73130af5498883356443549be3348688dc
星星魔法术 <xingxingmofashu@outlook.com>
2024-06-04 10:23:35 +0800

    fix auth0

--- server/plugins/auth0.ts
+++ server/plugins/auth0.ts
@@ -2,20 +2,32 @@ import { jwtVerify, createRemoteJWKSet } from 'jose'
 
 export default defineNitroPlugin(({ hooks }) => {
   hooks.hook('request', async (event) => {
-    
-    const jwt = event.node.req.headers.authorization
-
     const { auth0 } = useRuntimeConfig()
-
-    const JWKS = createRemoteJWKSet(new URL(`https://${auth0.domain}/.well-known/jwks.json`))
     try {
-      const { payload } = await jwtVerify(jwt, JWKS, {
-        audience: auth0.audience,
+
+      const jwt = event.node.req.headers.authorization
+      //const jwt = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlVZRHd1WngtRG0zczdOV3dxckxvYyJ9.eyJuaWNrbmFtZSI6Inhpbmd4aW5nbW9mYXNodSIsIm5hbWUiOiJ4aW5neGluZ21vZmFzaHVAb3V0bG9vay5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvMDhhMTgyOTFkZWI0NDA3OWY1MWNmMTBhZmE3YTdmYzA_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZ4aS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAyNC0wNi0wM1QwOTo0NDowMi43MjJaIiwiZW1haWwiOiJ4aW5neGluZ21vZmFzaHVAb3V0bG9vay5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6Ly9kZXYtdG92M2oyb3VvZzI3dmFxNi51cy5hdXRoMC5jb20vIiwiYXVkIjoiaWtBQzJlMm00akxDN1pzWkxOYm9VN2tlTmpNcjB0OGgiLCJpYXQiOjE3MTc0MDc4NDUsImV4cCI6MTcxNzQ0Mzg0NSwic3ViIjoiYXV0aDB8NjY1NTcwOTI3NGI0ZjZiMjRjMTMwNWIyIiwic2lkIjoidzFFMV9XWWpHQmhUQWozcFI3c2x0S3M4QzVpc2kwLW0iLCJub25jZSI6Ik5qVnZka1pKT1d0MFZTNStWbTVEWnpkT1lXWXdOMlIyZUVveFRHdGFTVWt6ZEdaNWVWbzJTSEUyVnc9PSJ9.DZX8KHb0JR-7NUcCkKyom2VZDoSbebqbc3sMdaRzBYLBhy8HwaQK6LkcSRiAPTfonJO8Sg21a6CS38l7-1QPGL3f_dGjGx70KuaEr1BRHtKevFnumxNeRnP-kcxA9ml-BgqGqYwKeGOOL-A2w5RP7lWnvHRmDsZ85KQpSOvLFKi8raOqGNipLjtykTJ7OZUwCKeCdheDrDpp-yUQQcYsjxg0e2T8hdwDmJGXawiUUeGNVDDRi8vK1iHaaQBop-u8PJevgyUVpbnR8UblKhQ5axg2HxLCi6OSSMNSBAM1Z4_yaR2iWXXq7SMO0oUI_Yam6l1XRHxweE3Dpy_qrRRVxA"
+      const JWKS = createRemoteJWKSet(new URL(`https://${auth0.domain}/.well-known/jwks.json`))
+
+      await jwtVerify(jwt, JWKS, {
+        audience: auth0.clientId,
         issuer: `https://${auth0.domain}/`,
       })
-      event.context.auth = payload
+      
     } catch (error) {
-      return sendError(event, createError({ statusCode: 401, statusMessage: 'Unauthorized' }))
+      console.log(error)
+      event.node.res.statusCode = 401
+    }
+  })
+
+  hooks.hook('beforeResponse', async (event) => {
+    switch (event.node.res.statusCode) {
+      case 500:
+        return sendError(event, createError({ statusCode: 500, statusMessage: 'Internal Server Error' }))
+      case 401:
+        return sendError(event, createError({ statusCode: 401, statusMessage: 'Unauthorized' }))
+      default:
+        return send(event)
     }
   })
 })
--- server/types/XBootResponse.ts
+++ server/types/XBootResponse.ts
@@ -1,22 +1,19 @@
-export enum ResponseStatusCode {
-  Error = 0,
+export enum StatusCode {
   Success = 200,
   BadRequest = 400,
   ApiUnauthorized = 401,
   NotFound = 404,
-  InternalServerError = 500,
-  NonAuthoritativeInformation = 5001,
-  RequiredError = 1001
+  InternalServerError = 500
 }
 
 export interface XBootResponse<T = any> {
-  code: ResponseStatusCode
+  code: StatusCode
   msg?: string
   success: boolean
-  data: T
+  data?: T
 }
 export interface XBootPageResponse<T = any> extends XBootResponse<T> {
-  total: number
-  pageIndex: number
-  pageCount: number
+  total?: number
+  pageIndex?: number
+  pageCount?: number
 }
